e9f0f53bd4615b9f1c35e8bb208f5aaffb578538
andressa turchetto <and.turchetto@gmail.com>
2024-06-26 17:54:20 -0300

    Ajustando a identação do código

--- src/app/shared/form-base/form-base.component.ts
+++ src/app/shared/form-base/form-base.component.ts
@@ -23,18 +23,30 @@ export class FormBaseComponent implements OnInit{
 
   ngOnInit(): void {
     this.cadastroForm = this.formBuilder.group({
-      nome: [null, Validators.required],
-      nascimento: [null, [Validators.required]],
-      cpf: [null, [Validators.required]],
-      cidade: [null, Validators.required],
-      email: [null, [Validators.required, Validators.email]],
-      senha: [null, [Validators.required, Validators.minLength(3)]],
-      genero: ['outro'],
-      telefone: [null, Validators.required],
-      estado: this.estadoControl,
-      confirmarEmail: [null, [Validators.required, Validators.email]],
-      confirmarSenha: [null, [Validators.required, Validators.minLength(3)]],
-      aceitarTermos: [null, [Validators.requiredTrue]]
+      nome:
+        [null, Validators.required],
+      nascimento:
+        [null, [Validators.required]],
+      cpf:
+        [null, [Validators.required]],
+      cidade:
+        [null, Validators.required],
+      email:
+        [null, [Validators.required, Validators.email]],
+      senha:
+        [null, [Validators.required, Validators.minLength(3)]],
+      genero:
+        ['outro'],
+      telefone:
+        [null, Validators.required],
+      estado:
+        this.estadoControl,
+      confirmarEmail:
+        [null, [Validators.required, Validators.email]],
+      confirmarSenha:
+        [null, [Validators.required, Validators.minLength(3)]],
+      aceitarTermos:
+        [null, [Validators.requiredTrue]]
     });
 
     this.FormularioService.setCadastro(this.cadastroForm)
